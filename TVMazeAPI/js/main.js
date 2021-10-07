d22bc6481858bc3d1956a3fc040d9981863a9d1f
Johannes KMD557 Mac <johannes.palojarvi@metropolia.fi>
2021-10-07 12:11:43 +0300

    modified tvmazeapi ui to be sleeker

--- TVMazeAPI/js/main.js
+++ TVMazeAPI/js/main.js
@@ -6,23 +6,33 @@ const hae = function (event) {
     event.preventDefault();
     $("#tulos").empty();
     $.getJSON(apiURL + $(this).serialize(), function (response) {
-        $("#tulos").append("<h1>Tulokset</h1>");
+        $("#tulos").append("<h1>Hakutulokset hakusanalle: \"" + $("#query").val() + "\"</h1>");
         if (response.length > 0) {
             $.each(response, function (indeksi, sarja) {
                 console.log(sarja.show);
                 $("#tulos").append(`<article>
+                <div class="row">
+                <div class="col">
     <h2>${sarja.show.name}</h2>
-    <p>Genre(s): ${sarja.show.genres}</p>
     
-    <a class ="btn btn-dark" href="${sarja.show.officialSite ? sarja.show.officialSite : sarja.show.url
-                    }">Linkki kotisivulle</a>
-    <figure>
-      <img src="${sarja.show.image ? sarja.show.image.medium : "tile.png"
+    <p>Genre${sarja.show.genres.length > 1 ? "s" : ""}: ${sarja.show.genres.join(', ')}</p>
+    </div>
+    </div>
+    <div class="row">
+    <figure class="col">
+    <a href=${sarja.show.officialSite ? sarja.show.officialSite : sarja.show.url}>
+      <img src="${sarja.show.image ? sarja.show.image.medium : "notfound.png"
                     }" alt="${sarja.show.name}">
+                    </a>
       <figcaption>${sarja.show.name}</figcaption>
     </figure>
-    <p>${sarja.show.summary}</p>
+   <div class="col"><p>${sarja.show.summary}</p></div>
+    </div>
+    <a class ="btn btn-dark" href="${sarja.show.officialSite ? sarja.show.officialSite : sarja.show.url
+                    }">Linkki kotisivulle</a>
+                    <br></br>
   </article>`);
+
             });
         } else {
             $("#tulos").append("<h1>Ei tuloksia</h1>");
